c9c9643359ffab01ba9eefee98af978c9fa3c6b3
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-10-09 22:10:36 -0700

    done

--- client/main.js
+++ client/main.js
@@ -7,10 +7,10 @@ const getGoalsBtn = document.getElementById("listOfGoals")
 const goalList = document.getElementById("unorganizedListOfGoals")
 
 const deleteGoal = (index) => {
-    axios.default.delete(`http://localhost:4000/api/goals/${index}`)
+    axios.delete(`http://localhost:4000/api/goals/${index}`)
 
     .then(goalFunction)
-        .catch
+    .catch(error => console.log(error));
     }
 
 const goalFunction = (response) => {
@@ -28,14 +28,12 @@ const goalFunction = (response) => {
     list.append(newList) 
     });
 }
-// const deleteBtn = document.getElementById("deleteButton")
-// let checkedBox = document.getElementsByClassName('delete-goal')
 
 
 
 
 const getCompliment = () => {
-    axios.default.get("http://localhost:4000/api/compliment/")
+    axios.get("http://localhost:4000/api/compliment/")
         .then(res => {
             const data = res.data;
             alert(data);
@@ -43,7 +41,7 @@ const getCompliment = () => {
 };
 
 const getFortune = () => {
-    axios.default.get("http://localhost:4000/api/fortune/")
+    axios.get("http://localhost:4000/api/fortune/")
         .then(res => {
             const data = res.data;
             alert(data);
@@ -53,7 +51,7 @@ const getFortune = () => {
 
 
 const getGoals = () => {
-    axios.default.get("http://localhost:4000/api/getgoals")
+    axios.get("http://localhost:4000/api/getgoals")
         .then(goalFunction)
     };
 
@@ -61,7 +59,7 @@ const addGoal = (event) => {
     event.preventDefault();
     let newGoal = input.value
     let body = {goal: newGoal}
-    axios.default.post("http://localhost:4000/api/goals/", body)
+    axios.post("http://localhost:4000/api/goals/", body)
     .then(goalFunction)
     .catch(error => console.log(error));
 };
@@ -70,24 +68,9 @@ const addGoal = (event) => {
 
 
 
-
 complimentBtn.addEventListener('click', getCompliment)
 fortuneBtn.addEventListener('click', getFortune)
 goalBtn.addEventListener('click', addGoal)
-getGoalsBtn.addEventListener('click', () =>{
-    axios.default.get("http://localhost:4000/api/getgoals")
-    .then((response) => {
-        const list = response.data
-        list.forEach(id => {
-            let goalEl = document.createElement('li')
-            goalEl.textContent = id
-            
-        goalList.appendChild(id)
-        })
-       
 
-    })
-    .catch(error => console.log(error));
-})
 
 getGoals();
